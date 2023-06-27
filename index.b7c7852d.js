2336ef648629bacb129f00284969f75e5fccdb13
SergrPashkov <128225830+SergrPashkov@users.noreply.github.com>
2023-06-27 22:14:58 +0000

    Deploying to gh-pages from @ SergrPashkov/PrivatBank_API@c1410c8b70051d0232177682184963bd8c568944 🚀

---
+++ index.b7c7852d.js
@@ -0,0 +1,2 @@
+fetch("https://api.privatbank.ua/p24api/pubinfo?exchange&coursid=5").then((function(n){return n.json()})).then((function(n){return console.log(n)}));
+//# sourceMappingURL=index.b7c7852d.js.map
---
+++ index.e15ea690.js
@@ -0,0 +1,2 @@
+fetch("https://api.privatbank.ua/p24api/pubinfo?exchange&coursid=5").then((n=>n.json())).then((n=>console.log(n)));
+//# sourceMappingURL=index.e15ea690.js.map
