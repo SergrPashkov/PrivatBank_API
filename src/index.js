6ec8e75f38e70ae46a23bef735141598893682b4
SergrPashkov <SGpashkovsg@gmail.com>
2023-06-28 10:42:03 +0300

    async

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
 // https://api.privatbank.ua/p24api/pubinfo?exchange&coursid=5
 // https://api.privatbank.ua/p24api/pubinfo?json&exchange&coursid=5
 
-// async function getExchangeRates() {
-// 	const URL = 'https://api.privatbank.ua/p24api/pubinfo';
-// 	const resp = await `${URL}`;
-// 	const data = await resp;
-// 	console.log(data);
-// }
+async function getExchangeRates() {
+	const URL = 'https://api.privatbank.ua/p24api/pubinfo';
+	const resp = await `${URL}`;
+	const data = await resp;
+	console.log(data);
+}
 
-// getExchangeRates()
+getExchangeRates()
 
-fetch('https://api.privatbank.ua/p24api/pubinfo?exchange&coursid=5')
-	.then((resp) => resp.json())
-	.then((json) => console.log(json))
+// fetch('https://api.privatbank.ua/p24api/pubinfo?exchange&coursid=5')
+// 	.then((resp) => resp.json())
+// 	.then((json) => console.log(json))
