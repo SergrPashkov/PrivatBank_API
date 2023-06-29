f23de40e3d8b7fec83eba3dda131698001b5815e
SergrPashkov <SGpashkovsg@gmail.com>
2023-06-30 00:28:02 +0300

    axios

--- src/server.php
+++ src/server.php
@@ -2,7 +2,7 @@
 
 if($_SERVER["REQUEST_METHOD"] === "GET"){
 	if($_GET["currency"] === "now"){
-		$data = file_get_contents('https://api.privatban');
+		$data = file_get_contents('https://api.privatbank.ua/p24api/pubinfo?json&exchange&coursid=5$');
 		$data_raw = json_decode($data, TRUE);
 
 		print_r(json_encode($data_raw));
